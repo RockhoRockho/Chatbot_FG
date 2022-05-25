48ee2dc30e3f4c6afbe32f0981faa3fc934fdcba
이준영 <89831302+RockhoRockho@users.noreply.github.com>
2022-05-25 21:46:41 +0900

    intent 모델

---
+++ models/intent/IntentModel.py
@@ -0,0 +1,42 @@
+import tensorflow as tf
+from tensorflow.keras.models import Model, load_model
+from tensorflow.keras import preprocessing
+
+
+# 의도 분류 모델 모듈
+class IntentModel:
+    def __init__(self, model_name, preprocess):
+
+        # 의도 클래스 별 레이블
+        self.labels = {0: "메뉴판 요구", 1: "메뉴 검색", 2: "추천메뉴", 3: "결제방법안내", 4: "쿠폰, 포인트",
+                       5: "원산지", 6: "시설, 위치", 7: "영업시간", 8: "텀블러", 9: "테이크아웃",
+                       10: "주문취소", 11: "욕설"}
+
+        # 의도 분류 모델 불러오기
+        self.model = load_model(model_name)
+
+        # 챗봇 Preprocess 객체
+        self.p = preprocess
+
+
+    # 의도 클래스 예측
+    def predict_class(self, query):
+        # 형태소 분석
+        pos = self.p.pos(query)
+
+        # 문장내 키워드 추출(불용어 제거)
+        keywords = self.p.get_keywords(pos, without_tag=True)
+        sequences = [self.p.get_wordidx_sequence(keywords)]
+
+        # 단어 시퀀스 벡터 크기
+        from config.GlobalParams import MAX_SEQ_LEN
+
+        # 패딩처리
+        padded_seqs = preprocessing.sequence.pad_sequences(sequences, maxlen=MAX_SEQ_LEN, padding='post')
+
+        predict = self.model.predict(padded_seqs)
+        predict_class = tf.math.argmax(predict, axis=1)
+        return predict_class.numpy()[0]
+
+
+

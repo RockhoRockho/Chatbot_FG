54e56918bfb8744c88da3820eced7ef247e55079
Jungmic <102244947+Jungmic@users.noreply.github.com>
2022-06-02 09:50:50 +0900

    Orderitems

---
+++ utils/OrderItems.py
@@ -0,0 +1,43 @@
+# 학습 데이터 초기화
+
+class OrderItems():
+    
+    def __init__(self, db):
+        self.db = db
+        
+    
+    def all_clear_train_data(db):
+        # 기존 학습 데이터 삭제
+        sql = '''
+                delete from order_item
+            '''
+        with db.cursor() as cursor:
+            cursor.execute(sql)
+
+        # auto increment 초기화
+        sql = '''
+        ALTER TABLE order_item
+        '''
+        with db.cursor() as cursor:
+            cursor.execute(sql)
+
+
+    # db에 데이터 저장
+    def insert_data(db, pk, order_id, product_id, option_id, count):
+        pk_db = pk
+        order_id_db = order_id
+        product_id_db = product_id
+        option_id_db = option_id
+        count_db = count
+
+        sql = '''
+            INSERT order_item(id, order_id, product_id, option_id, count) 
+            values('%s', '%s', '%s', '%s', '%s')
+        ''' % (pk_db.value, order_id_db, product_id_db.value, option_id_db.value, count_db.value)
+
+        # 엑셀에서 불러온 cell에 데이터가 없는 경우, null 로 치환
+        sql = sql.replace("'None'", "null")
+
+        with db.cursor() as cursor:
+            cursor.execute(sql)
+            db.commit()

c43c80322dd27286dbee89a11dd9a7f82f2716ee
Peter-Duwon-Kim <bwk2037@gmail.com>
2022-06-02 10:58:37 +0900

    주문취소 수정

--- utils/OrderCancel.py
+++ utils/OrderCancel.py
@@ -29,9 +29,9 @@ class OrderCancel():
         order_id_db = order_id
 
         sql = '''
-            DELETE FROM `order_item` WHERE `product_id` = product_id AND `order_id` = order_id
+            DELETE FROM `order_item` WHERE `product_id` = %s AND `order_id` = %s
 
-        ''' 
+        ''' % (product_id_db.value, order_id_db.value)
 
         # 엑셀에서 불러온 cell에 데이터가 없는 경우, null 로 치환
         sql = sql.replace("'None'", "null")

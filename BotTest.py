db6efe0de40512a7955878515a7fad3063289225
Leyhara <vocalold@naver.com>
2022-06-03 11:03:19 +0900

    test

--- BotTest.py
+++ BotTest.py
@@ -40,7 +40,7 @@ def set_intent_values(predict, name, ner):
     global ner_predicts
     ner_predicts = ner
     
-def answer_short(db):
+def answer_short(db, query):
     global answer
     global answer_image
     global intent_predict
@@ -55,7 +55,9 @@ def answer_short(db):
     word_8 = ['테이크아웃', '테이크 아웃']
     word_10 = ['라떼', '커피', '음료', '식사']
 
-    if query:
+    question = word_0 + word_2 + word_3 + word_4 + word_5 + word_6 + word_7 + word_8 + word_10
+    
+    if query in question:
         try:
             ## Word 0
             if query in word_0:
@@ -137,11 +139,19 @@ def answer_short(db):
             answer = "죄송해요 무슨 말인지 모르겠어요. 조금 더 공부 할게요."
             answer_image = None
             
+        return answer
+    else:
+        set_intent_values(404, 'Error', '')
+        answer = "죄송해요 무슨 말인지 모르겠어요. 조금 더 공부 할게요."
+        return answer
+            
 def to_client(conn, addr, params):
     db = params['db']
     
     try:
         db.connect()
+        
+        read = conn.recv(2048)
         
         print('===========================')
         print('Connection from: %s' % str(addr))
@@ -159,8 +169,42 @@ def to_client(conn, addr, params):
         price = recv_json_data['Price']
         option = recv_json_data['Option']
         
-        answer_short(db);
+        print(answer_short(db, query));
+
+        # 검색된 답변데이터와 함께 앞서 정의한 응답하는 JSON 으로 생성
+        send_json_data_str = {
+            "Query" : query,
+            "Answer" : answer,
+            "AnswerImageUrl" : answer_image,
+            "Intent" : intent_name,
+            "NER" : str(ner_predicts),
+            "State" : 0,
+            "Product" : 0,
+            "Price" : 0,
+            "Count" : 1,
+            "Option" : 0,
+            "Detail" : 0,
+        }
         
+        # State 값 확인하여 
+        if intent_predict == 1:
+            send_json_data_str["Product"] = product
+            send_json_data_str["Price"] = price
+            send_json_data_str["Option"] = option
+            send_json_data_str["State"] = int(intent_predict)
+        elif intent_predict == 9:
+            send_json_data_str["Product"] = product
+            send_json_data_str["State"] = int(intent_predict)
+        elif intent_predict == 10:
+            send_json_data_str["Detail"] = answer_detail
+        
+        # 디버깅
+        print(send_json_data_str)
+        
+        # json 텍스트로 변환. 하여 전송
+        message = json.dumps(send_json_data_str)
+        conn.send(message.encode())  # utf-8 인코딩하여 클라이언트에 전송
+
     except Exception as ex:
         print(ex)
         
@@ -168,3 +212,33 @@ def to_client(conn, addr, params):
         if db is not None:
             db.close()
         conn.close()
+        
+if __name__ == '__main__':
+    # 질문/답변 학습 디비 연결 객체 생성
+    db = Database(
+        host=DB_HOST, user=DB_USER, password=DB_PASSWORD, db_name=DB_NAME
+    )
+    print("DB 접속")
+    
+    port = 5050
+    listen = 100
+    
+    # 봇 서버 동작
+    bot = BotServer(port, listen)
+    bot.create_sock()
+    print("cafebot start")
+    
+    # 무한루프를 돌면서 챗봇 클라이언트의 요청(연결)을 기다린다(리스닝!)
+    while True:
+        conn, addr = bot.ready_for_client()  # 서버 연결 요청이 서버에서 수락되면, 곧바로 챗봇 클라이언트 서비스 요청 처리하는 쓰레드 생성
+        
+        params = {
+            "db" : db
+        }
+        client = threading.Thread(target=to_client, args=(
+            conn, # 클라이언트 연결 소켓
+            addr, # 클라이언트 연결 주소 정보
+            params  # 쓰레드 내부에서 DB에 접근할 수 있도록 넘겨줌
+        ))
+        
+        client.start()  # 쓰레드 시작, 위 target 함수가 별도의 쓰레드에 실려 실행된다.

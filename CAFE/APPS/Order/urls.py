9b0f12b0b7c7fe028b1270a4a3a9705e202699a1
이준영 <89831302+RockhoRockho@users.noreply.github.com>
2022-06-08 20:25:02 +0900

    order views, urls

--- CAFE/APPS/Order/urls.py
+++ CAFE/APPS/Order/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from APPS.Order import views
 
 urlpatterns = [
-    
+    path('list/', views.order_list, name='order_list'),
 ]
--- CAFE/APPS/Order/views.py
+++ CAFE/APPS/Order/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
+def order_list(request):
+    return render(request, 'order_list.html')

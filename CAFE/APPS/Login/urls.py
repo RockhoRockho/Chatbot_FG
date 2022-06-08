51f5511aba8f7d677f21def7f29c7fd91f6cec01
이준영 <89831302+RockhoRockho@users.noreply.github.com>
2022-06-08 13:05:20 +0900

    로그인 백엔드

--- CAFE/APPS/Login/urls.py
+++ CAFE/APPS/Login/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from APPS.Login import views
 
 urlpatterns = [
-    path('register/', views.register, name='register'),
+    path('', views.login, name='login'),
+    path('join/', views.join, name='join'),
+    path('logout/', views.logout, name='logout'),
+    path('needlogin/', views.needlogin, name='needlogin'),
 ]
--- CAFE/APPS/Login/views.py
+++ CAFE/APPS/Login/views.py
@@ -1,7 +1,69 @@
-from django.shortcuts import render
-from django.core.paginator import Paginator
+from django.shortcuts import redirect, render
+from APPS.Login.models import User
 
-# Create your views here.
-def register(request): {
-    
-}
+def login(request):
+    context = {
+        'error': '',
+    }
+    if request.method == 'GET':
+        return render(request, 'login.html', context)
+    elif request.method == 'POST':
+        uid = request.POST.get('uid', None)
+        pw = request.POST.get('password', None)
+
+        if not uid and not pw:
+            context['error'] = "아이디와 비밀번호를 모두 입력하세요."
+        else:
+            try:
+                user = User.objects.get(user_id=uid)
+            except User.DoesNotExist:
+                user = None
+
+            if not user:
+                context['error'] = '없는 계정입니다.'
+            elif not pw == user.pw:
+                context['error'] = '비밀번호가 틀립니다.'
+            else:
+                context['User'] = request.session.get('User')
+                request.session['User'] = user.user_id
+                return redirect('/')
+        return render(request, 'login.html', context)
+
+def logout(request):
+    if request.session.get('User'):
+        del(request.session['User'])
+
+    context = {
+        'error': '',
+    }
+
+    return render(request, 'logout.html', context)
+
+def join(request):
+    context = {
+        'User': request.session.get('User'),
+    }
+
+    if request.method =='GET':
+        return render(request, 'join.html', context)
+    elif request.method == 'POST':
+        uid = request.POST['uid']
+        pw = request.POST['pw']
+
+        try:
+            user = User.objects.get(user_id=uid)
+        except User.DoesNotExist:
+            user = None
+
+        if user:
+            context['error'] = '이미 존재하는 아이디입니다.';
+            return render(request, 'join.html', context)
+        else:
+            userT = User(
+                uid = uid,
+                pw= pw)
+            userT.save() 
+        return render(request, 'login.html')
+
+def needlogin(request):
+    return render(request, "needlogin.html")

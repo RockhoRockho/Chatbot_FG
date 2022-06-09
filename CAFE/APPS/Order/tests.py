9fe466200b2a372656d40e31e7d5880b1b4ab94c
이준영 <89831302+RockhoRockho@users.noreply.github.com>
2022-06-09 11:51:34 +0900

    utils 제거

--- CAFE/APPS/Order/tests.py
+++ CAFE/APPS/Order/tests.py
@@ -1,3 +1,103 @@
 from django.test import TestCase
+import pymysql 
 
-# Create your tests here.
+def ProductOption():
+    
+
+    db = pymysql.connect(
+            host=DB_HOST,
+            user=DB_USER,
+            passwd=DB_PASSWORD,
+            db=DB_NAME,
+            charset='utf8'
+        )
+
+    sql = ''' 
+    SELECT * from product_option
+    '''
+    with db.cursor() as cur:
+        cur.execute(sql)
+        option = cur.fetchall()
+
+    return option
+
+def OrderDetail():
+    # DB 가져오기
+    DB_HOST = "localhost"
+    DB_USER = "myuser118"
+    DB_PASSWORD = "1234"
+    DB_NAME = "mydb118"
+
+    db = None
+
+    db = pymysql.connect(
+            host=DB_HOST,
+            user=DB_USER,
+            passwd=DB_PASSWORD,
+            db=DB_NAME,
+            charset='utf8'
+        )
+
+    sql = ''' 
+        SELECT * from order_detail
+    '''
+
+    with db.cursor() as cur:
+        cur.execute(sql)
+        order_detail = cur.fetchall()
+
+    return order_detail
+
+def OrderItem():
+    # DB 가져오기
+    DB_HOST = "localhost"
+    DB_USER = "myuser118"
+    DB_PASSWORD = "1234"
+    DB_NAME = "mydb118"
+
+    db = None
+
+    db = pymysql.connect(
+            host=DB_HOST,
+            user=DB_USER,
+            passwd=DB_PASSWORD,
+            db=DB_NAME,
+            charset='utf8'
+        )
+
+    sql = ''' 
+    SELECT * from order_item
+    '''
+
+    with db.cursor() as cur:
+        cur.execute(sql)
+        order_item = cur.fetchall()
+
+    return order_item
+
+def Product():
+    # DB 가져오기
+    DB_HOST = "localhost"
+    DB_USER = "myuser118"
+    DB_PASSWORD = "1234"
+    DB_NAME = "mydb118"
+
+    db = None
+
+    db = pymysql.connect(
+            host=DB_HOST,
+            user=DB_USER,
+            passwd=DB_PASSWORD,
+            db=DB_NAME,
+            charset='utf8'
+        )
+
+    sql = ''' 
+        SELECT * from product_cafe
+    '''
+
+    with db.cursor() as cur:
+        cur.execute(sql)
+        product = cur.fetchall()
+
+    return product
--- CAFE/APPS/Order/views.py
+++ CAFE/APPS/Order/views.py
@@ -1,30 +1,74 @@
 from django.shortcuts import render
-from utils.Product import Product
-from utils.OrderDetail import OrderDetail
-from utils.OrderItem import OrderItem
-from utils.ProductOption import ProductOption
+import pymysql 
 
+# DB 가져오기
+DB_HOST = "localhost"
+DB_USER = "myuser118"
+DB_PASSWORD = "1234"
+DB_NAME = "mydb118"
+
+db = None
+
+db = pymysql.connect(
+            host=DB_HOST,
+            user=DB_USER,
+            passwd=DB_PASSWORD,
+            db=DB_NAME,
+            charset='utf8'
+        )
+
+sql = ''' 
+    SELECT * from order_detail
+'''
 dtl_li = []
-for i in OrderDetail():
+with db.cursor() as cur:
+    cur.execute(sql)
+    order_detail = cur.fetchall()
+
+for i in order_detail:
     pk, _ = i
     dtl_li.append(pk)
 
+sql = ''' 
+    SELECT * from product_cafe
+'''
+
+with db.cursor() as cur:
+    cur.execute(sql)
+    product = cur.fetchall()
+
 prd_dic = {}
-for i in Product():
+for i in product:
     pk, name, price, _, _, _, _, _, _, _ = i
     prd_dic[pk] = [name, price]
 
+sql = ''' 
+SELECT * from product_option
+'''
+with db.cursor() as cur:
+    cur.execute(sql)
+    option = cur.fetchall()
+
 opt_dic = {}
-for i in ProductOption():
+for i in option:
     pk, _, opt_name = i
     opt_dic[pk] = opt_name
 
+sql = ''' 
+SELECT * from order_item
+'''
+
+with db.cursor() as cur:
+    cur.execute(sql)
+    order_item = cur.fetchall()
+
+
 list = []
-for i in OrderItem():
+for i in order_item:
     pk, order_id, product_id, option_id, count = i
     list.append([order_id, prd_dic[product_id], opt_dic[option_id], count])
 
-print(list)
+db.close()
 
 def order_list(request):
     return render(request, 'order_list.html')

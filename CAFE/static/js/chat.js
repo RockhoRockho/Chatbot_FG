36165248a3c970895a7f68847ed099b839736b81
yoo-wonjun <85600188+yoo-wonjun@users.noreply.github.com>
2022-06-10 09:28:28 +0900

    채팅배경색 및 텀블러 테이크아웃답변

--- CAFE/static/js/chat.js
+++ CAFE/static/js/chat.js
@@ -23,7 +23,7 @@ function send_message(){
     }
 
     // 입력한 채팅 화면에 출력
-    const addtext = "<div style='margin:15px 0;text-align:right;'> <span style='padding:3px 10px;background-color:#3388cc;border-radius:3px;'>" + chattext + "</span></div>";
+    const addtext = "<div style='margin:15px 0;text-align:right;'> <span style='padding:3px 10px;background-color:#E0B88A;border-radius:3px;'>" + chattext + "</span></div>";
     $("#chatbox").append(addtext);
 
     // 먼저 입력했던 것은 지우기
@@ -53,20 +53,20 @@ function send_message(){
             if (response.Intent == '메뉴'){ // 상품 리스트 뽑을 때
                 for (let i = 0; i < response.AnswerImageUrl.length; i++){
                     const bottext =
-                        "<div style='margin:15px 0;text-align:left; max-width:70%;'><div style='padding:3px 10px;background-color:#DDD;border-radius:3px; display:inline-block; word-break: keep-all;'>" +
+                        "<div style='margin:15px 0;text-align:left; max-width:70%;'><div style='padding:3px 10px;background-color: #F5F5DC;border-radius:3px; display:inline-block; word-break: keep-all;'>" +
                         response.Answer[i] + "<image src='/static/img/" + response.AnswerImageUrl[i] + "'></image>"+ response.Detail[i] +
                         "</div></div><br>";
                     $chatbox.append(bottext);
                 }
             }  else if (response.Intent == '주문' && response.AnswerImageUrl != null){ //상품 하나 뽑을 때
                 const bottext =
-                    "<div style='margin:15px 0;text-align:left; max-width:70%;'><div style='padding:3px 10px;background-color:#DDD;border-radius:3px; display:inline-block; word-break: keep-all;'>" +
+                    "<div style='margin:15px 0;text-align:left; max-width:70%;'><div style='padding:3px 10px;background-color:#F5F5DC;border-radius:3px; display:inline-block; word-break: keep-all;'>" +
                     response.Answer + "<image src='/static/img/" + response.AnswerImageUrl + "'></image>" + response.Detail +
                     "</div></div>";
                 $chatbox.append(bottext);
             }  else{ // 텍스트만 뽑을 때
                 const bottext =
-                    "<div style='margin:15px 0;text-align:left; max-width:70%;'><div style='padding:3px 10px;background-color:#DDD;border-radius:3px; display:inline-block; word-break: keep-all;'>" +
+                    "<div style='margin:15px 0;text-align:left; max-width:70%;'><div style='padding:3px 10px;background-color:#F5F5DC;border-radius:3px; display:inline-block; word-break: keep-all;'>" +
                     response.Answer +
                     "</div></div>";
                 $chatbox.append(bottext);

90c81910f3a61c441cfdad580ad9acf83105dc20
yoo-wonjun <85600188+yoo-wonjun@users.noreply.github.com>
2022-06-17 16:22:33 +0900

    카카오팝업 다시 커밋

--- CAFE/static/js/chat.js
+++ CAFE/static/js/chat.js
@@ -388,5 +388,5 @@ function send_option(optionNm){
     
 }
 function openPop(){
-    window.open('chatbot/kakaopay/', '카페','top=10%, left=20%, height=800px, width=600px')
+    window.open('chatbot/kakaopay/', '카페','top=10%, left=20%, height=800px, width=800px')
 }
